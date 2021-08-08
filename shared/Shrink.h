1431c2551ad0066fa292b6f0b4251a360d46bd1e
Sinflower <s1nflower@yandex.com>
2021-08-08 20:25:45 +0200

    r196  - Fixed Deepl  - Added missing Bing request information

--- shared/Shrink.h
+++ shared/Shrink.h
@@ -40,7 +40,7 @@
 
 // Simple way of ensuring a clean build before release.
 #define APP_NAME L"Translation Aggregator"
-#define APP_VERSION L"2021.07.17 r195 (Unofficial)"
+#define APP_VERSION L"2021.08.08 r196 (Unofficial)"
 
 #define HTTP_REQUEST_ID L"TRAG/" APP_VERSION
 
